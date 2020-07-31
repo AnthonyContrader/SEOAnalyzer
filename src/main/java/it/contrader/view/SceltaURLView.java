986c2c61a532ff140538dd8fb54087779a9b6cd6
riccardomarin99 <ric.marin@libero.it>
2020-07-31 14:25:54 +0200

    Risolto il range di URL usati nella cronologia

--- src/main/java/it/contrader/view/SceltaURLView.java
+++ src/main/java/it/contrader/view/SceltaURLView.java
@@ -9,15 +9,17 @@ public class SceltaURLView extends AbstractView{
 	String URL; 
 	Request request;
 	final String REGEX = "^(http|https|ftp)\\://[a-zA-Z0-9\\-\\.]+\\.[a-zA-Z]{2,3}(:[a-zA-Z0-9]*)?/?([a-zA-Z0-9\\-\\._\\?\\,\\'/\\\\\\+&amp;%\\$#\\=~])*$";
-
+	int i;
 	@Override
 	public void showResults(Request request) {
 		//show la lista
 		System.out.println("-------------LISTA URL------------\n");
 		this.request = request;
-		for(int i=1; i<6; i++)
+		for(i=1; i<6; i++)
 		{
-			System.out.println(i + ") " + this.request.get("URL"+i));
+			if(this.request.get("URL"+i)!=null)
+				System.out.println(i + ") " + this.request.get("URL"+i));
+			else break;
 		}
 	}
 
@@ -30,8 +32,30 @@ public class SceltaURLView extends AbstractView{
 
 	@Override
 	public void submit() {
-
-		switch (choice) {
+		int temp = Integer.parseInt(choice);
+		if(temp>=1 && temp<=i-1)
+		{
+			System.out.println(temp + " " + i);
+			URL = (String) request.getString("URL"+ temp);
+			request.put("URL", URL);
+			MainDispatcher.getInstance().callAction("URL", "doControl", request);
+		}
+		else {
+			System.out.println("test");
+			URL = new String(choice);
+			boolean match = URL.matches(REGEX);
+			while( !match ) {
+				System.out.println("ATTENZIONE! URL scritta in modo errato. Riprovare");
+				System.out.println("Versione corretta es: \"http://www.example.com/~joe\"");
+				URL = this.getInput();
+				match = URL.matches(REGEX);
+			}
+			
+			request.put("URL", URL);
+			MainDispatcher.getInstance().callAction("URL", "doControl", request);
+		}
+		
+		/*switch (choice) {
 		
 		case "1":
 			URL = (String) request.getString("URL1");
@@ -71,7 +95,7 @@ public class SceltaURLView extends AbstractView{
 			request.put("URL", URL);
 			MainDispatcher.getInstance().callAction("URL", "doControl", request);
 			break;
-		}
+		}*/
 	}
 
 }
