25f65fb86136f05e96ed4725028eb5bd679f15f2
riccardomarin99 <ric.marin@libero.it>
2020-08-27 11:50:16 +0200

    fix

--- src/main/java/it/contrader/test.java
+++ src/main/java/it/contrader/test.java
@@ -4,20 +4,30 @@ import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import it.contrader.dao.UserRepository;
+import it.contrader.dto.UserDTO;
 import it.contrader.model.User;
 import it.contrader.model.User.Usertype;
+import it.contrader.service.AbstractService;
 import it.contrader.session.SessionHibernate;
 
+@Service
 
-public class test {
+public class test extends AbstractService<User, UserDTO> {
 
+	@Autowired
+	private static testHibernate hibernate;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SessionFactory factory = SessionHibernate.getSessionfactory();
 		Session session = factory.openSession();
 		
 		User u = new User();
+		User u2 = new User();
 		u.setUsername("mario");
 		u.setPassword("rossi");
 		u.setUsertype(Usertype.USER);
@@ -29,6 +39,9 @@ public class test {
 		
 		Session session2 = factory.openSession();
 		session2.beginTransaction();
+		u2.setUsername("mario");
+		u2 = hibernate.findByUsernameAndPassword("mario", "rossi");
+		System.out.println(u2.getUsername().toString());
 		List<User> l = (List<User>)session2.createQuery("from User").list();
 		for(User us: l)
 		{
